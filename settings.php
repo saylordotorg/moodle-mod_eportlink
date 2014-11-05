4a597c0af1af22fdb027d9928548d2535c227601
John Azinheira <john.azinheira@saylor.org>
2014-11-05 11:46:03 -0500

    Remove unnecessary observer class and settings

--- settings.php
+++ settings.php
@@ -19,11 +19,11 @@
 * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 defined('MOODLE_INTERNAL') || die;
-
+/*
 if ($ADMIN->fulltree) {
 	$settings->add(
 		new admin_setting_configtext('eport_secret_key', get_string('secretkeylabel', 'eportlink'), get_string('secretkeyhelp', 'eportlink'), '', PARAM_TEXT)
 	 	//$CFG->eport_secret_key
 	);
-}
+}*/
 ?>
